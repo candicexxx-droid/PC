c8c349e83f90e6f95b39e0a712df5e65d750d93d
Candice Cai <caixy.candice2016@outlook.com>
2022-08-04 16:37:35 -0700

    loglikelihood_k_ones works

--- main.jl
+++ main.jl
@@ -12,67 +12,73 @@ include("utils.jl")
 
 
 
-# function run()
+function run()
     train_cpu, test_cpu = mnist_cpu()
-    # train_gpu, test_gpu = mnist_gpu()
-    println("hi")
+    train_gpu, test_gpu = move_to_gpu(train_cpu,test_cpu)
+
     # twenty_dataset_names
     dataset_id=1
 
-    train_cpu_t, valid_data, test_cpu_t = twenty_datasets(twenty_dataset_names[dataset_id])
+    # train_cpu_t, valid_data, test_cpu_t = twenty_datasets(twenty_dataset_names[dataset_id])
     
-    train_cpu=Matrix(train_cpu_t)
-    test_cpu = Matrix(test_cpu_t)
-    train_cpu = train_cpu[1:20, 1:20]
-    latents = 5
+    # train_cpu=Matrix(train_cpu_t)
+    # # [1:20,1:5]
+    # test_cpu = Matrix(test_cpu_t)
+    # [1:20,1:5]
+
+    train_gpu,test_gpu = move_to_gpu(train_cpu,test_cpu)
+
+
+    latents = 64
     pseudocount = 0.01
+
+
     
     println("Generating HCLT structure with $latents latents... ");
     @time pc = hclt(train_cpu, latents; pseudocount, input_type = Literal);
     init_parameters(pc; perturbation = 0.4);
 
-    
-    println("Number of free parameters: $(num_parameters(pc))")
     println("Number of nodes: $(num_nodes(pc)) before conversion ")
-    TikzPictures.standaloneWorkaround(true)  # workaround
-    z=plot(pc);
-    save(PDF("plot_before"), z);
-    convert_product_to_binary(pc)
-
+    
+    added=convert_product_to_binary(pc)
+    println("total Number of added nodes: $(added) after conversion ")
     println("Number of nodes: $(num_nodes(pc)) after conversion ")
 
-    TikzPictures.standaloneWorkaround(true)  # workaround
-    z=plot(pc);
-    save(PDF("plot_after"), z);
+    
 
 
-    # print("Moving circuit to GPU... ")
-    # CUDA.@time bpc = CuBitsProbCircuit(pc) #move circuit to gpu
 
-    batch_size  = 512
+    print("Moving circuit to GPU... ")
+    CUDA.@time bpc = CuBitsProbCircuit(pc) #move circuit to gpu
+
+    batch_size  = 1024
     pseudocount = .005
     softness    = 0
     n = num_nodes(pc)
     k = maximum(sum(Int.(train_cpu),dims=2))
-
+    println("n: $(n); k: $(k)")
     ll=loglikelihood_k_ones(pc,n,k)
 
-    
-    # print("First round of minibatch EM... ")
-    # CUDA.@time mini_batch_em(bpc, train_gpu, 100; batch_size, pseudocount, 
-    # 			 softness, param_inertia = 0.01, param_inertia_end = 0.95)
+    println("initial loglikelihood is $(ll)")
+    print("First round of minibatch EM... ")
+    CUDA.@time mini_batch_em(bpc, train_gpu, 100; batch_size, pseudocount, 
+    			 softness, param_inertia = 0.01, param_inertia_end = 0.95)
     			 
-    # CUDA.@time mini_batch_em(bpc, train_gpu, 100; batch_size, pseudocount, 
-    # 			 softness, param_inertia = 0.95, param_inertia_end = 0.999)
+    CUDA.@time mini_batch_em(bpc, train_gpu, 100; batch_size, pseudocount, 
+    			 softness, param_inertia = 0.95, param_inertia_end = 0.999)
     
-    # CUDA.@time full_batch_em(bpc, train_gpu, 10; batch_size, pseudocount, softness)
+    CUDA.@time full_batch_em(bpc, train_gpu, 10; batch_size, pseudocount, softness)
 
-    # print("Update parameters... ")
-    # @time ProbabilisticCircuits.update_parameters(bpc)
+    print("Update parameters... ")
+    @time ProbabilisticCircuits.update_parameters(bpc)
+    
+    ll=loglikelihood_k_ones(pc,n,k)
+
+    println("final loglikelihood is $(ll)")
     pc
 
 
-# end
+end
 
 
-# run()
+run()
--- utils.jl
+++ utils.jl
@@ -34,8 +34,8 @@ function mnist_cpu()
 end
 
 
-function mnist_gpu()
-    cu.(mnist_cpu())
+function move_to_gpu(train,test)
+    CuArray(train),CuArray(test)
 end
 
 function enumerate_partitions(num_child, k)
@@ -56,27 +56,34 @@ function enumerate_partitions(num_child, k)
     return results
 end
 
-
+function softmax(x) 
+    y= exp.(x) ./    
+sum(exp.(x))
+end
 
 function loglikelihood_k_ones(root::ProbCircuit,n, k; Float=Float32)
-    println("Using modified loglikelihood")
-    
-    # cache = Dict()
+
     f_i(node) = begin
         result = Array{Union{Float64, Nothing}, 1}(nothing, (k+1))
-        result[1] = 0
-        result[2] = 1
+        if node.dist.value
+            result[1] = log(0)
+            result[2] = log(1)
+        else
+            result[1] = log(1)
+            result[2] = log(0)
+        end
+
+        
         result
-        # val = data[example_id, first(randvars(node))
-        # ismissing(val) ? Float(0.0) : loglikelihood(dist(node), val)
     end
     f_s(node, ins) = begin #ins -> a vector of children outpus, each element of vector is of type Array{Union{Float64, Nothing}, 1}
         result = Array{Union{Float64, Nothing}, 1}(nothing, (k+1))
-        flag=true
+        
         for i in 0:k #mapping: 0~k -> 1~k+1
-            # child_result_l = ins[1]
-            # child_result_r = ins[2]
+
+            flag=true
             for child_result in ins
+                # println("child_result[$(i+1)]: $(child_result[i+1])")
                 if(child_result[i+1]==nothing)
                     flag=false
                 end
@@ -87,6 +94,7 @@ function loglikelihood_k_ones(root::ProbCircuit,n, k; Float=Float32)
                 result[i+1] = reduce(logsumexp, node.params .+ child_sum) 
             end                  
         end
+        #  println("sum node: $(result)")
         result
         # reduce(logsumexp, node.params .+ ins) #sum node     
     end
@@ -96,32 +104,45 @@ function loglikelihood_k_ones(root::ProbCircuit,n, k; Float=Float32)
         for i in 0:k #mapping: 0~k -> 1~k+1
             child_result_l = ins[1]
             child_result_r = ins[2]
-            if child_result_l[i+1]!=nothing && child_result_r[k-i+1]!=nothing
-                child_sum = (child_result_l[i+1],child_result_r[k-i+1])
-                result[i+1] = sum(child_sum)
-            end                  
+            # println("left child:$(child_result_l)")
+            # println("right child: $(child_result_r)")
+            temp=[-Inf]
+            for j in 0:i
+                if child_result_l[j+1]!=nothing && child_result_r[i-j+1]!=nothing
+                    child_sum = child_result_l[j+1]+child_result_r[i-j+1]
+                    # println("childsum $(child_sum)")
+                    append!(temp,child_sum)
+                end  
+
+            end
+            # println("temp $(temp)")
+            result[i+1] = logsumexp(temp)
+                            
         end
+        #  println("prod node: $(result)")
         result
     end
     
     # sum(ins) #product node 
     
-    foldup_aggregate(root, f_i, f_m, f_s, Array{Union{Float64, Nothing}, 1})
+    final=foldup_aggregate(root, f_i, f_m, f_s, Array{Union{Float64, Nothing}, 1})
+    println("final array is $(final)")
+    final=logsumexp(final)
 end
 
 # k = 8
 # children = 3
 
 function convert_product_to_binary(root::ProbCircuit)
-
+    total_added=0
     f_i(node) = begin
-        1
+        0
     end
     f_m(node, ins) = begin
         
         function convert_bin(inputs)    
             if length(inputs) == 2 #base case
-                println("base!")
+                # println("base!")
                 bin_mul = multiply(inputs)
             else 
                 # println("length of input is $(length(inputs))")
@@ -130,19 +151,27 @@ function convert_product_to_binary(root::ProbCircuit)
             end
             
         end
+        # added = length(node.inputs)>2 ? (length(node.inputs)-2) : 0
+        # added = sum(ins) + added
         if length(node.inputs)>2
-            println("number of children of this prod node is $(length(node.inputs))")
-            println("should add  $(length(node.inputs)-2) nodes")
+            # println("number of children of this prod node is $(length(node.inputs))")
+            # println("should add  $(length(node.inputs)-2) nodes")
             # println(node.inputs)
+            total_added+=length(node.inputs)-2
             node.inputs = [node.inputs[1],convert_bin(node.inputs[2:end])]
-            println("after conversion: number of children of this prod node is $(length(node.inputs))")
+            # println("after conversion: number of children of this prod node is $(length(node.inputs))")
         end
-        1
+        
+        0
+        # println("added $(added)")
+        # return added
     end
     f_s(node, ins) = begin
-        1
+        added = sum(ins)
+
     end
     foldup_aggregate(root, f_i, f_m, f_s,Int64)
+    total_added
 end
 
 
@@ -152,19 +181,29 @@ if abspath(PROGRAM_FILE) == @__FILE__
     X2=InputNode(2, Literal(true))
     X3=InputNode(3, Literal(true))
     X4=InputNode(4, Literal(true))
-    pc=multiply([X1,X2,X3,X4])
-    pc.inputs
+
+    # pc=summate([multiply([X1,X2,X3,X4]),multiply([X1,X2,X3,X4])])
+    pc=summate([multiply([X1,X2,X3]),multiply([X1,X2,X3])])
+    # pc = multiply([X1,X2,X3,X4])
+
+    init_parameters(pc);
+
     println("Number of free parameters: $(num_parameters(pc))")
     println("Number of nodes: $(num_nodes(pc)) before conversion ")
-    TikzPictures.standaloneWorkaround(true)  # workaround
-    z=plot(pc);
-    save(PDF("plot_before"), z);
-    convert_product_to_binary(pc)
+    # TikzPictures.standaloneWorkaround(true)  # workaround
+    # z=plot(pc);
+    # save(PDF("plot_before"), z);
+    added = convert_product_to_binary(pc)
+
+    println("total Number of added nodes: $(added) after conversion ")
 
     println("Number of nodes: $(num_nodes(pc)) after conversion ")
 
-    TikzPictures.standaloneWorkaround(true)  # workaround
-    z=plot(pc);
-    save(PDF("plot_after"), z);
+    # TikzPictures.standaloneWorkaround(true)  # workaround
+    # z=plot(pc);
+    # save(PDF("plot_after"), z);
+
+    v=loglikelihood_k_ones(pc,3,3)
+    println("loglikelihood of <= k is $(v)")
 end
 # X1, X2, X3 = literals(ProbCircuit, 3)
